ea0fc30df97331b6b4c448288fc02f3a08c4327f
ivan3848 <57994520+ivan3848@users.noreply.github.com>
2024-04-17 17:01:49 -0400

    Add Tailwind and next ui

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,82 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { Avatar, Dropdown, DropdownItem, DropdownMenu, DropdownTrigger, Input, Link, Navbar, NavbarBrand, NavbarContent, NavbarItem } from "@nextui-org/react";
+import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0)
+	return (
+		<>
+			<Navbar isBordered>
+				<NavbarContent justify="start">
+					<NavbarBrand className="mr-4">
+						{/* <AcmeLogo /> */}
+						<p className="hidden sm:block font-bold text-inherit">ACME</p>
+					</NavbarBrand>
+					<NavbarContent className="hidden sm:flex gap-3">
+						<NavbarItem>
+							<Link color="foreground" href="#">
+								Features
+							</Link>
+						</NavbarItem>
+						<NavbarItem isActive>
+							<Link href="#" aria-current="page" color="secondary">
+								Customers
+							</Link>
+						</NavbarItem>
+						<NavbarItem>
+							<Link color="foreground" href="#">
+								Integrations
+							</Link>
+						</NavbarItem>
+					</NavbarContent>
+				</NavbarContent>
 
-  return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+				<NavbarContent as="div" className="items-center" justify="end">
+					<Input
+						classNames={{
+							base: "max-w-full sm:max-w-[10rem] h-10",
+							mainWrapper: "h-full",
+							input: "text-small",
+							inputWrapper:
+								"h-full font-normal text-default-500 bg-default-400/20 dark:bg-default-500/20",
+						}}
+						placeholder="Type to search..."
+						size="sm"
+						startContent={ "search" }
+						type="search"
+					/>
+					<Dropdown placement="bottom-end">
+						<DropdownTrigger>
+							<Avatar
+								isBordered
+								as="button"
+								className="transition-transform"
+								color="secondary"
+								name="Jason Hughes"
+								size="sm"
+								src="https://i.pravatar.cc/150?u=a042581f4e29026704d"
+							/>
+						</DropdownTrigger>
+						<DropdownMenu aria-label="Profile Actions" variant="flat">
+							<DropdownItem key="profile" className="h-14 gap-2">
+								<p className="font-semibold">Signed in as</p>
+								<p className="font-semibold">zoey@example.com</p>
+							</DropdownItem>
+							<DropdownItem key="settings">My Settings</DropdownItem>
+							<DropdownItem key="team_settings">Team Settings</DropdownItem>
+							<DropdownItem key="analytics">Analytics</DropdownItem>
+							<DropdownItem key="system">System</DropdownItem>
+							<DropdownItem key="configurations">Configurations</DropdownItem>
+							<DropdownItem key="help_and_feedback">
+								Help & Feedback
+							</DropdownItem>
+							<DropdownItem key="logout" color="danger">
+								Log Out
+							</DropdownItem>
+						</DropdownMenu>
+					</Dropdown>
+				</NavbarContent>
+			</Navbar>
+		</>
+	);
 }
 
-export default App
+export default App;
